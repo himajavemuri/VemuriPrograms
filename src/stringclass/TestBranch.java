134b08a3b3499d8007d0c61e6be67c05497e9aa2
himaj <himaj@192.168.0.8>
2020-09-13 00:51:16 +0530

    hi how r u

---
+++ src/stringclass/TestBranch.java
@@ -0,0 +1,13 @@
+package stringclass;
+
+public class TestBranch 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Derived data type");
+		System.out.println("Go to hell sys out");
+
+	}
+
+}
